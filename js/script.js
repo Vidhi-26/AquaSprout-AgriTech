239e4b1d916f5b2901171f1b0c6d999c11835c7a
Dr Neelima <drneelima@Drs-MacBook-Air.phub.net.cable.rogers.com>
2021-08-29 02:26:40 -0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function () { 
+    
+        /*Hide navbar when clicked outside*/
+        $(document).click(function(event) {
+            $("#entire-navbar").collapse('hide');
+         });
+});
+    
+   
